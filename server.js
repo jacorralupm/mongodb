662fb8ad1abb780ad830dc102af026581664e3c1
jacorralupm <ja.corral@alumnos.upm.es>
2017-06-15 16:04:39 +0200

    Create server.js

--- server.js
+++ server.js
@@ -37,4 +37,4 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 
 // launch ======================================================================
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('La magia ocurre en el puerto ' + port+':)');
